39f6f96cde200bd74067a7a9b15916ce41c129dd
David Gomez <dgomez@iconcologia.net>
2018-06-13 14:58:58 +0200

    Add map plotting features
    - EPSG4326 projection
    - Parameterized region bounds and tolerances
    - Changed dot threshold to relative size
    - Add pol_area to shp file
    - Fix errors on palette configuration

--- reportcompiler_ic_tools/maps.py
+++ reportcompiler_ic_tools/maps.py
@@ -21,50 +21,88 @@ __all__ = ['generate_map', 'DEFAULT_TOLERANCES', 'DOT_THRESHOLD',
 PROJECTION_DICT = {
     'robinson': {
         'proj': 'robin'
-    }
+    },
+    'epsg4326': {}
 }
 ''' Pyproj projections. '''
 
 REGION_BOUNDS = {
-    'XWX': (
-        [-16000000, 8000000],
-        [16000000, -6000000]
-    ),
-    'XFX': (
-        [-1580000, 3800000],
-        [5580000, -4040000]
-    ),
-    'XMX': (
-        [-14500000, 8000000],
-        [-1000000, -5700000]
-    ),
-    'XSX': (
-        [2500000, 5800000],
-        [13000000, -970000]
-    ),
-    'XEX': (
-        [-2500000, 8200000],
-        [5000000, 3800000]
-    ),
-    'XOX': (  # In EPSG:4326 coordinates
-        [110.049387, 23.214162],
-        [-118.454180 + 360, -51.583946, ]
-    ),
+    'robinson': {
+        'XWX': (
+            [-16000000, 8000000],
+            [16000000, -6000000]
+        ),
+        'XFX': (
+            [-1580000, 3800000],
+            [5580000, -4040000]
+        ),
+        'XMX': (
+            [-14500000, 8000000],
+            [-1000000, -5700000]
+        ),
+        'XSX': (
+            [2500000, 5800000],
+            [13000000, -970000]
+        ),
+        'XEX': (
+            [-2500000, 8200000],
+            [5000000, 3800000]
+        ),
+        'XOX': None  # Uncapable of wrapping in this projection, disabled
+    },
+    'epsg4326': {
+        'XWX': (
+            [-163, 80],
+            [163, -60]
+        ),
+        'XFX': (
+            [-25, 38],
+            [56, -38]
+        ),
+        'XMX': (
+            [-165, 80],
+            [-15, -60]
+        ),
+        'XSX': (
+            [28, 57],
+            [155, -16]
+        ),
+        'XEX': (
+            [-32, 80],
+            [103, 35]
+        ),
+        'XOX': (  # In EPSG:4326 coordinates
+            [110.049387, 23.214162],
+            [-118.454180 + 360, -51.583946, ]
+        )
+    }
 }
-''' Robinson coordinates, except Oceania (EPSG:4326). '''
+''' Region bounds by projection coordinates '''
 
 DEFAULT_TOLERANCES = {
-    'XWX': 40000,
-    'XFX': 26000,
-    'XMX': 40000,
-    'XSX': 32000,
-    'XEX': 13000,
-    'XOX': .4,  # In EPSG:4326 coordinates
+    'robinson': {
+        'XWX': 40000,
+        'XFX': 26000,
+        'XMX': 40000,
+        'XSX': 32000,
+        'XEX': 13000,
+        'XOX': None  # Uncapable of wrapping in this projection, disabled
+    },
+    'epsg4326': {
+        'XWX': .6,
+        'XFX': .2,
+        'XMX': .4,
+        'XSX': .2,
+        'XEX': .1,
+        'XOX': .4,  # In EPSG:4326 coordinates
+    }
 }
-''' Default tolerances for polygon reduction in different regions. '''
+''' Default tolerances for polygon simplification in different regions by
+projection. '''
 
-DOT_THRESHOLD = 35
-''' A dot will be plotted for countries with areas below this value. '''
+DOT_THRESHOLD = .00001
+''' A dot will be plotted for countries with areas below this percentage of
+the total shown map area. '''
 
 
 def generate_map(data,
@@ -92,8 +130,8 @@ def generate_map(data,
         ggplot corresponding color scale (continuous or discrete).
     :param bool plot_na_dots: Whether to plot the dots for small countries
         if said country doesn't have data available.
-    :param int tolerance: Coordinate tolerance for polygon complexity
-        reduction, a higher number will result in simpler polygons and faster
+    :param int tolerance: Coordinate tolerance for polygon simplification,
+        a higher number will result in simpler polygons and faster
         rendering (see DEFAULT_TOLERANCES).
     :param int plot_size: Size of the plot, which determines the relative sizes
         of the elements within.
@@ -107,17 +145,20 @@ def generate_map(data,
     :returns: a ggplot-like plot with the map
     :rtype: plotnine.ggplot
     """
+    if projection not in PROJECTION_DICT.keys():
+        raise ValueError('Projection "{}" not valid'.format(projection))
+
     if scale_params is None:
         scale_params = {}
 
-    if region not in REGION_BOUNDS:
+    if region not in REGION_BOUNDS[projection]:
         raise ValueError(
             '"region" not available. Valid regions are: {}'.format(
-                ', '.join(REGION_BOUNDS.keys())
+                ', '.join(REGION_BOUNDS[projection].keys())
             ))
 
     if tolerance is None:
-        tolerance = DEFAULT_TOLERANCES[region]
+        tolerance = DEFAULT_TOLERANCES[projection][region]
 
     countries = GeoDataFrame.from_file(
         os.path.join(os.path.dirname(__file__), 'data/world-countries.shp'))
@@ -136,11 +177,17 @@ def generate_map(data,
         countries.loc[XOX_countries, 'lon'] = [c.x for c in centroids]
         countries.loc[XOX_countries, 'lat'] = [c.y for c in centroids]
     else:
-        countries = countries.to_crs(PROJECTION_DICT[projection])
+        if projection != 'epsg4326':
+            countries = countries.to_crs(PROJECTION_DICT[projection])
+            centroids = countries.apply(
+                lambda row: row['geometry'].centroid,
+                axis=1)
+            countries['lon'] = [c.x for c in centroids]
+            countries['lat'] = [c.y for c in centroids]
 
     countries['geometry'] = countries['geometry'].simplify(tolerance)
 
-    upper_left, lower_right = REGION_BOUNDS[region]
+    upper_left, lower_right = REGION_BOUNDS[projection][region]
     limits_x = [upper_left[0], lower_right[0]]
     limits_y = [lower_right[1], upper_left[1]]
     ratio = (limits_x[1] - limits_x[0]) / (limits_y[1] - limits_y[0])
@@ -150,7 +197,13 @@ def generate_map(data,
                          how='left',
                          left_on='iso',
                          right_on=iso_field)
-    plot_data['plot_dot'] = (plot_data['area'] < DOT_THRESHOLD)
+    map_bounds = REGION_BOUNDS['epsg4326'][region]
+    map_area = (
+        (map_bounds[1][0] - map_bounds[0][0]) *
+        (map_bounds[0][1] - map_bounds[1][1])
+    )
+    plot_data['plot_dot'] = (plot_data['pol_area'] <
+                             DOT_THRESHOLD * map_area)
 
     if not plot_na_dots:
         plot_data['plot_dot'] &= ~pd.isnull(plot_data[value_field])
@@ -214,17 +267,22 @@ def generate_map(data,
                       color=line_color) +
            scale_x_continuous(breaks=[], limits=limits_x) +
            scale_y_continuous(breaks=[], limits=limits_y) +
-           fill_scale +
-           scale_color_manual(name=' ',
-                              values=[line_color],
-                              breaks=[False],
-                              labels=['No data available']) +
            theme(figure_size=(plot_size*ratio, plot_size),
                  panel_background=element_rect(fill='white', color='black'),
-                 panel_border=element_rect(fill='white', color='black')
+                 panel_border=element_rect(fill='white',
+                                           color='black',
+                                           size=.1)
                  ) +
            xlab('') + ylab('')
           )
+
+    if len(plot_data_values.index) > 0:
+        plt += fill_scale
+
+    plt += scale_color_manual(name=' ',
+                              values=[line_color],
+                              breaks=[False],
+                              labels=['No data available'])
 
     if plot_data[value_field].dtype == 'object':
         plt += guides(fill=guide_legend(override_aes={'shape': None}))
